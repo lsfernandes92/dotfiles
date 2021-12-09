9f565dc9a39cc0733806cccbb3e08f9bb37c0432
lsfernandes92 <lsfernandes92@gmail.com>
2021-12-09 13:22:55 -0300

    Update docker aliases
    These aliases meant facilitate so I don't need to remember all the 
    commands that I used everytime to build a brand new Rails container

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,3 +3,9 @@ alias dc='docker-compose $*'
 alias dcu="docker-compose up -d"
 alias dcd="docker-compose down"
 alias dsa='docker stop $(docker ps -a -q)'
+alias step1='docker-compose run --no-deps web rails new . --force --database=postgresql'
+alias step2='sudo chown -R $USER:$USER .'
+alias step3='docker-compose build'
+alias step4='rm -f config/database.yml && mv database.yml config/'
+alias step5='docker-compose up'
+alias step6='docker-compose run web rake db:create'
