05ab3109f3a8b2da142bcd95daf0b738b417305c
Lucas Fernandes <lsfernandes92@gmail.com>
2022-06-10 11:01:50 -0300

    Update Recurly commands

--- recurly/aliases.zsh
+++ recurly/aliases.zsh
@@ -1,5 +1,11 @@
+# Recurly-cli commands
 alias r='recurly $*'
 alias rr='recurly run $*'
+alias rrr='recurly run recurly-app $*'
+alias rrrr='recurly run recurly-app rspec $*'
 alias rb='recurly run recurly-app bash'
 alias rrails='recurly run recurly-app rails'
 alias rfullrestart='recurly down && recurly system down && recurly pull && recurly up'
+
+# Google cloud cli command
+alias gclogin='gcloud auth login --update-adc'
