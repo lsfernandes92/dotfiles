606eb164eb9ad6ecbf83946127617f883dd14331
lsfernandes92 <lsfernandes92@gmail.com>
2026-02-05 14:52:45 -0300

    clean system aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,38 +1,21 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -ln --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
-
-# Shortcuts
 alias ip="ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'"
-alias dotfiles="code ~/.dotfiles"
-alias sonicpi-audio-settings="code ~/.sonic-pi/config/audio-settings.toml"
+alias dotfiles="$EDITOR ~/.dotfiles"
+alias sonicpi-configs="$EDITOR ~/.sonic-pi/config/audio-settings.toml"
 alias update-brew='brew update; brew upgrade; brew cleanup'
 alias update-software='sudo softwareupdate -ia --verbose'
 alias update!='update-brew && update-software && mas upgrade'
 alias icloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs'
 alias backup='mtmrbackup && vscodebackup'
 
-# Navigation
-alias cls="clear"
-alias ..="cd .."
-alias home="cd"
-alias desenv="cd ~/Documents/desenv"
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-
-# cli
-# cmatrix is a command-line utility that displays a scrolling "Matrix"-style
-# digital rain effect in your terminal, inspired by the movie "The Matrix." It is
-# popular among Linux and Unix users for its visual effect.
+# cli utility that displays
+# a scrolling matrix-style
+# digital rain effect in terminal
 # 
-# GitHub repository:
 # https://github.com/abishekvashok/cmatrix
 alias matrix="cmatrix -s"
+
+# navigation shortcuts
+alias cls="clear"
+alias home="cd"
+alias desenv="cd ~/Documents/desenv"
+alias l="ls -aln"
